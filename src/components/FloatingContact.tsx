e55a8288cc21c3084224b00e4f4bdcb32538aaeb
4DeR99 <moulmado@student.1337.ma>
2024-04-28 00:02:08 +0100

    feat: adding side contact section

---
+++ src/components/FloatingContact.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+// import
+
+function FloatingContact() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <img src="" alt="" />
+        </li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
+}
+
+export default FloatingContact;
