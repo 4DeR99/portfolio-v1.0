0cab4b58bd71818c6c364ef6c436d62343e6d5f1
4DeR99 <moulmado@student.1337.ma>
2024-04-27 18:00:21 +0100

    chore: added assets

---
+++ src/assets/colors.ts
@@ -0,0 +1,9 @@
+export const colors = {
+  background: '#1A1A24',
+  offBackground: '#08080B',
+  primary: '#588FCA',
+  secondary: '#1C647D',
+  tertiary: '#4B3B6D',
+  text: '#FFFFFF',
+  offText: '#A3A3A7',
+};
