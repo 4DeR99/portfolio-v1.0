11fdccac5683e3a7e0555a551ad7b394aebb4864
4DeR99 <moulmado@student.1337.ma>
2024-04-28 21:48:26 +0100

    feat: added paragraphs in the about section

--- src/assets/consts.ts
+++ src/assets/consts.ts
@@ -1,3 +1,8 @@
 export const constants = {
-  about: '',
+  leet: 'My coding jorney begane in 2017 in high school where I learned some algorithms, after high school I joined 1337 coding school in 2021 where I learned the basics of programming I spent 2 years building awsome projects with C and C++ like Minishell, Webserv, Cub3D and more, I got into web developement last year when I built my first web project that was the last project in the common core before you go out on an internship, I built it with NextJs and NestJs I had lots of fun building it with my team.',
+  oraTech:
+    'I joined Ora Technologies in 2023 as a full stack developer, I was working on a project that was built with React native and NestJs, however after 2 months the company decided to switch to flutter so I had to learn flutter and build the project from scratch, I had a great time working with the team and I learned a lot from them.',
+  leavingOra:
+    'Unfortunately after my internship period ended the company had some finantial issues and they had to let me go, It was a great experience and I learned a lot from them.',
+  after: 'After I kept learning more about web and mobile developement ',
 };
--- src/components/organisms/About.tsx
+++ src/components/organisms/About.tsx
@@ -1,15 +1,19 @@
+import { constants } from 'assets/consts';
 import React from 'react';
 
 function About() {
+  console.log(constants);
   return (
-    <div className="flex flex-col h-[70vh]">
-      <div className="flex flex-col w-11/12 mx-auto my-auto lg:w-3/6 lg:m-auto">
-        <h1>About</h1>
-        <p>
-          My name is Mohammed Reda Oulmadou and I'm a software engineer
-          specialized in mobile and web development. I'm a pationate problem
-          solver and a fast learner and I enjoy competetive programing.
-        </p>
+    <div className="flex flex-col p-5 gap-8">
+      <div className="flex flex-col items-center w-11/12 mx-auto my-auto lg:w-3/6 lg:m-auto gap-[5em]">
+        <h1 className="text-4xl font-extrabold">
+          _about(<span className="text-primary">"ME"</span>)
+        </h1>
+        <div className="flex flex-col gap-10">
+          <p>{constants.leet}</p>
+          <p>{constants.oraTech}</p>
+          <p>{constants.leavingOra}</p>
+        </div>
       </div>
     </div>
   );
