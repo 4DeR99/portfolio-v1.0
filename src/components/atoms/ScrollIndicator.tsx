dac3faeaab9e973553e6ec4f46c9b608e17f80f0
4DeR99 <moulmado@student.1337.ma>
2024-04-28 18:42:25 +0100

    feat: added invisibility for scroll indicator when scrolling down

--- src/components/atoms/ScrollIndicator.tsx
+++ src/components/atoms/ScrollIndicator.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import 'assets/styles/ScrollIndicator.css';
 
 interface ScrollIndicatorProps {
@@ -6,7 +6,27 @@ interface ScrollIndicatorProps {
 }
 
 function ScrollIndicator({ className }: ScrollIndicatorProps) {
-  return <div className={`mouse-scroll ${className}`} />;
+  const scrollRef = useRef<HTMLDivElement>(null);
+  const [hidden, setHidden] = React.useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (scrollRef.current) {
+        const { top } = scrollRef.current.getBoundingClientRect();
+        setHidden(top < 400);
+      }
+    };
+
+    document.addEventListener('scroll', handleScroll);
+    return () => document.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  return (
+    <div
+      className={`mouse-scroll ${className} ${hidden ? 'invisible' : ''}`}
+      ref={scrollRef}
+    />
+  );
 }
 
 export default ScrollIndicator;
