588dd26da279dcb3cd43f8fd87224996e1bf013f
4DeR99 <moulmado@student.1337.ma>
2024-04-28 18:43:42 +0100

    feat: added about me section and some constants

---
+++ src/components/organisms/About.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div className="flex flex-col h-[70vh]">
+      <div className="flex flex-col w-11/12 mx-auto my-auto lg:w-3/6 lg:m-auto">
+        <h1>About</h1>
+        <p>
+          My name is Mohammed Reda Oulmadou and I'm a software engineer
+          specialized in mobile and web development. I'm a pationate problem
+          solver and a fast learner and I enjoy competetive programing.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Layout from 'components/layout/main';
 import Hero from 'components/organisms/Hero';
+import About from 'components/organisms/About';
 
 function Home() {
   return (
     <Layout>
       <Hero />
+      <About />
     </Layout>
   );
 }
