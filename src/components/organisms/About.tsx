3c371cb1c6a054d22ecc5b65867a00b7096ec5e6
4DeR99 <moulmado@student.1337.ma>
2024-04-29 10:33:51 +0100

    feat: about section now takes the full screen height and shows the data from "constant"

--- src/components/organisms/About.tsx
+++ src/components/organisms/About.tsx
@@ -2,17 +2,18 @@ import { constants } from 'assets/consts';
 import React from 'react';
 
 function About() {
-  console.log(constants);
   return (
-    <div className="flex flex-col p-5 gap-8">
-      <div className="flex flex-col items-center w-11/12 mx-auto my-auto lg:w-3/6 lg:m-auto gap-[5em]">
+    <div className="flex flex-col p-5 gap-8 h-screen">
+      <div className="flex flex-col items-center w-11/12 mx-auto my-auto lg:w-3/6 lg:m-auto gap-[4em]">
         <h1 className="text-4xl font-extrabold">
           _about(<span className="text-primary">"ME"</span>)
         </h1>
-        <div className="flex flex-col gap-10">
-          <p>{constants.leet}</p>
-          <p>{constants.oraTech}</p>
-          <p>{constants.leavingOra}</p>
+        <div className="flex flex-col gap-4">
+          {constants.about.split('-').map((paragraph, index) => (
+            <p key={index} className="">
+              {paragraph}
+            </p>
+          ))}
         </div>
       </div>
     </div>
