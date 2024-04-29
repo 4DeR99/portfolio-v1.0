b82e7746bafa951afa82db3d1ac295a1e87d8dd9
4DeR99 <moulmado@student.1337.ma>
2024-04-30 00:34:20 +0100

    feat: added experience section

---
+++ src/components/molecule/Experience.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import OraTech from './OraTech';
+
+function Experience() {
+  return (
+    <div className="flex flex-col items-center gap-8 w-11/12 mx-auto xl:w-[1000px] lg:mx-auto">
+      <h2 className="text-2xl font-medium lg:self-start">.workExperience()</h2>
+      <div className="flex flex-col lg:flex-row lg:flex-wrap gap-2 w-full ">
+        <OraTech />
+      </div>
+    </div>
+  );
+}
+
+export default Experience;
--- src/components/organisms/Resume.tsx
+++ src/components/organisms/Resume.tsx
@@ -1,11 +1,15 @@
-import Skills from 'components/molecule/Skills';
 import React from 'react';
+import Skills from 'components/molecule/Skills';
+import Experience from 'components/molecule/Experience';
 
 function Resume() {
   return (
     <div className="flex flex-col p-5 gap-16 items-center min-h-screen">
       <h1 className="text-4xl font-extrabold text-center">_resume()</h1>
-      <Skills />
+      <div className="flex flex-col gap-40">
+        <Skills />
+        <Experience />
+      </div>
     </div>
   );
 }
