2a7d4e2e2c14ee3db2382638583216a159298070
4DeR99 <moulmado@student.1337.ma>
2024-04-29 14:04:09 +0100

    chore: added skills data

---
+++ src/assets/skills.ts
@@ -0,0 +1,70 @@
+export const skills = {
+  frontend: {
+    html: {
+      name: 'HTML',
+      icon: 'assets/icons/html.svg',
+    },
+    css: {
+      name: 'CSS',
+      icon: 'assets/icons/css.svg',
+    },
+    react: {
+      name: 'React',
+      icon: 'assets/icons/react.svg',
+    },
+    tailwind: {
+      name: 'Tailwind',
+      icon: 'assets/icons/tailwind.svg',
+    },
+    next: {
+      name: 'Next.js',
+      icon: 'assets/icons/next.svg',
+    },
+  },
+  backend: {
+    node: {
+      name: 'Node.js',
+      icon: 'assets/icons/node.svg',
+    },
+    nest: {
+      name: 'Nest.js',
+      icon: 'assets/icons/nest.svg',
+    },
+    postgesql: {
+      name: 'PostgreSQL',
+      icon: 'assets/icons/postgresql.svg',
+    },
+  },
+  mobile: {
+    flutter: {
+      name: 'Flutter',
+      icon: 'assets/icons/flutter.svg',
+    },
+    reactNative: {
+      name: 'React Native',
+      icon: 'assets/icons/react.svg',
+    },
+  },
+  languages: {
+    c: {
+      name: 'C',
+      icon: 'assets/icons/c.svg',
+    },
+    cpp: {
+      name: 'C++',
+      icon: 'assets/icons/cpp.svg',
+    },
+    javascript: {
+      name: 'JavaScript',
+      icon: 'assets/icons/javascript.svg',
+    },
+    typescript: {
+      name: 'TypeScript',
+      icon: 'assets/icons/typescript.svg',
+    },
+    dart: {
+      name: 'Dart',
+      icon: 'assets/icons/dart.svg',
+    },
+  },
+};
