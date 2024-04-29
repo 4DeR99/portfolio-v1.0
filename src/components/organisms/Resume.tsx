dafec33df87d858affb68889d104fab2e36cf102
4DeR99 <moulmado@student.1337.ma>
2024-04-29 12:29:02 +0100

    feat: created a Resume section files and working on putting everything together

---
+++ src/components/organisms/Resume.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Resume() {
+  return (
+    <div className="flex flex-col p-5 gap-8 border-white border-[1px]">
+      <h1 className="text-4xl font-extrabold">_resume()</h1>
+    </div>
+  );
+}
+
+export default Resume;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Layout from 'components/layout/main';
 import Hero from 'components/organisms/Hero';
 import About from 'components/organisms/About';
+import Resume from 'components/organisms/Resume';
 
 function Home() {
   return (
     <Layout>
       <Hero />
       <About />
+      <Resume />
     </Layout>
   );
 }
