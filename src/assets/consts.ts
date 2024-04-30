fa8c35214fc5f6aa098b8ffc2df913060280b56f
4DeR99 <moulmado@student.1337.ma>
2024-05-01 00:28:36 +0100

    chore: added navbar data to consts

--- src/assets/consts.ts
+++ src/assets/consts.ts
@@ -6,6 +6,13 @@ export interface Project {
   demo?: string;
 }
 
+export const navItems = [
+  { name: '_about()', id: 'about' },
+  { name: '_resume()', id: 'resume' },
+  { name: '_projects()', id: 'projects' },
+  { name: '_contact()', id: 'contact' },
+];
+
 export const about =
   'I was always passionate about computers and electronic devices since I was 7, always wondering how these things inside the screen move and how to make them yourself.-My coding journey began in 2017 in high school, where I learned some algorithms. After high school, I joined 1337 coding school in 2021, where I learned the basics of programming. I spent 2 years building awesome projects with C and C++ like Minishell, Webserv, Cub3D, and more. I got into web development last year when I built my first web project with NextJs and NestJs. I had lots of fun building it with my team. That was the last project in the common core before going out on an internship.-During my internship, I developed my skills in web development, learned a lot of new technologies, and got introduced to mobile development using Flutter. It was a great experience learning and working with the team, and I am looking forward to learning more and building more awesome projects.';
 
