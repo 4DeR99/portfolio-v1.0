548f85141cfa0ffa845350d393901a76dd46ebdb
4DeR99 <moulmado@student.1337.ma>
2024-04-30 17:10:13 +0100

    chore: update some configs in hoverButton

--- src/components/atoms/HoverButton.tsx
+++ src/components/atoms/HoverButton.tsx
@@ -3,10 +3,12 @@ import React from 'react';
 interface HoverButtonProps {
   hoverBg: string;
   buttonTitle: string;
+  minWidth?: string;
 }
 
-function HoverButton({ hoverBg, buttonTitle }: HoverButtonProps) {
-  const className = `flex justify-center ${hoverBg} text-white px-4 py-2 rounded-md min-w-[100px] text-sm`;
+function HoverButton({ hoverBg, buttonTitle, minWidth }: HoverButtonProps) {
+  minWidth = minWidth || 'min-w-[100px]';
+  const className = `flex justify-center ${hoverBg} text-white px-4 py-2 rounded-md ${minWidth} text-sm`;
   return <div className={className}>{buttonTitle}</div>;
 }
 
